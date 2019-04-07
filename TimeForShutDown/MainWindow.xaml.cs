feee8e23b86e7205fe0a9015cd244be0ee7a1520
DresBezNogi <szarymichal93@gmail.com>
2019-04-07 16:31:20 +0200

    add Timer Class

---
+++ TimeForShutDown/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeForShutDown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Timer myTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myTimer = new Timer();
+            this.DataContext = myTimer;
+           
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myTimer.Start();
+        }
+    }
+
+}
---
+++ TimeForShutDown/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace TimeForShutDown
+{
+    public class Timer : INotifyPropertyChanged
+    {
+        private string currentTime;
+        public string CurrentTime
+        {
+            get { return this.currentTime; }
+            set
+            {
+                if (value != this.currentTime)
+                {
+                    this.currentTime = value;
+                    NotifyPropertyChange("CurrentTime");
+                }
+            }
+        }
+        private TimeSpan startTime;
+        private DispatcherTimer timer;
+
+        public Timer()
+        {
+            startTime = new TimeSpan(0, 0, 0);
+            this.currentTime = String.Format("{0:hh\\:mm\\:ss}", startTime);
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickTimer;
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        private void tickTimer(object sender, EventArgs e)
+        {
+
+            startTime = startTime + new TimeSpan(0, 0, 1);
+            CurrentTime = String.Format("{0:hh\\:mm\\:ss}", startTime);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChange(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+
+    }
+}
